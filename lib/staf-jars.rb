e5379a5eb254d6c26377b9df9bf6d58e8e01ffd7
joey.he <joey.he8x@qq.com>
2013-08-26 11:03:46 +0800

    fix bugs

--- lib/staf-jars.rb
+++ lib/staf-jars.rb
@@ -1,11 +1,11 @@
 if RUBY_PLATFORM =~ /java/
-  require 'staf-jars/commons-logging.jar'
-  require 'staf-jars/logback-core-1.0.6.jar'
-  require 'staf-jars/gson-1.6.jar'
-  require 'staf-jars/ojdbc14.jar'
-  require 'staf-jars/JSTAF.jar'		   
-  require 'staf-jars/slf4j-api-1.6.6.jar'
-  require 'staf-jars/logback-classic-1.0.6.jar'
-  require 'staf-jars/staf_cmd_java.jar'
+  require 'commons-logging.jar'
+  require 'logback-core-1.0.6.jar'
+  require 'gson-1.6.jar'
+  require 'ojdbc14.jar'
+  require 'JSTAF.jar'		   
+  require 'slf4j-api-1.6.6.jar'
+  require 'logback-classic-1.0.6.jar'
+  require 'staf_cmd_java.jar'
 
 end
